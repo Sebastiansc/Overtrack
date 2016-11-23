cbb637aab80d7e5706e652d02a017aab08bd970e
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-23 00:36:14 -0800

    Add yml config file for New Relic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
 # ...
 
   # Full error reports are disabled and caching is turned on.
