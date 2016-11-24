b98bc3ca2ccb93c767f7eb44c9f0ab195e149161
Sonik Jhang <sonik.jhang@utexas.edu>
2016-11-23 17:02:49 -0800

    player action update

--- frontend/actions/player_actions.js
+++ frontend/actions/player_actions.js
@@ -1,7 +1,13 @@
 import UPDATE_PLAYER from 'UPDATE_PLAYER';
+import FETCH_PLAYER from 'FETCH_PLAYER';
 
 
 export const updatePlayer = player => ({
   type: UPDATE_PLAYER,
+  player
+});
+
+export const fetchPlayer = player => ({
+  type: FETCH_PLAYER,
   player
 });
