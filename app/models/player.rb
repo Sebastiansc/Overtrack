f1f302c7e06848e0e4dbd520debb99b1ba230fc6
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-23 17:47:02 -0800

    Add players table. Set Json deserializing

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  store :quick, accessors: [ :melee, :attack ], coder: JSON
+  store :competitive, accessors: [ :melee, :attack ], coder: JSON
 end
--- db/migrate/20161122031411_create_players.rb
+++ db/migrate/20161122031411_create_players.rb
@@ -3,7 +3,11 @@ class CreatePlayers < ActiveRecord::Migration[5.0]
     create_table :players do |t|
       t.string :username, null: false
       t.integer :level, null: false
-
+      t.text :avatar, null: false
+      t.text :level_frame, null: false
+      t.text :star, null: false
+      t.text :quick, null: false
+      t.text :competitive, null: false
       t.timestamps
     end
   end
