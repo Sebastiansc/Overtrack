5aca3b6c10e74ed143787fec7f85e35af867f72c
Sonik Jhang <sonik.jhang@utexas.edu>
2016-11-25 23:28:07 -0800

    working Summoner model

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -12,26 +12,29 @@ class Summoner < ApplicationRecord
     profile = HTTParty.get(
       "https://#{region}.api.pvp.net/api/lol/#{region}/v1.4/summoner/by-name/#{summoner_name}?api_key=#{api_key}"
     )
-    profile_entry = summoner_entry(profile[summoner_id], region)
+    return false if profile["statusCode"] == 404
+    profile = profile.to_h[summoner_name]
+    profile_entry = summoner_entry(profile["id"], region).to_h[profile["id"].to_s]
     profile_info = {
       summoner_id: profile["id"],
       level: profile["summonerLevel"],
       name: profile["name"],
-      profile_icon: profile["profile_icon_id"]
+      profile_icon: profile["profileIconId"]
     }
     @summoner = Summoner.new(profile_info)
     solo_rank(profile_entry)
+    @summoner.save!
   end
 
   def self.summoner_entry(summoner_id, region)
     HTTParty.get(
-      "https://#{region}.api.pvp.net/api/lol/#{region}/v2.5/league/by-summoner/#{summoner_id}entry?api_key=#{api_key}"
+      "https://#{region}.api.pvp.net/api/lol/#{region}/v2.5/league/by-summoner/#{summoner_id}/entry?api_key=#{api_key}"
     )
   end
 
   def self.solo_rank(profile_entry)
     profile_entry.each do |profile|
-      next unless profile.queue == "RANKED_SOLO_5x5"
+      next unless profile["queue"] == "RANKED_SOLO_5x5"
       @summoner.tier = profile["tier"]
       @summoner.league_name = profile["name"]
       @summoner.wins = profile["entries"].first["wins"]
