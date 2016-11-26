75625af3d686edcbc3f2572149116fa7a0549063
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-26 08:23:39 -0800

    Remove old raketasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,7 +21,3 @@
 
 set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-
-every 1.day, at: '3:00 am' do
-  rake "fetcher:update"
-end
