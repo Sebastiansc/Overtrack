42728dee5e5b0bec562d783e2aa72783ca3c38cf
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-25 22:33:32 -0800

    Add model relations

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,3 +1,6 @@
 class Summoner < ApplicationRecord
   validates :name, :level, :summoner_id, :profile_icon, :tier, :wins, :loses, :league_points, :league_name, :division, presence: true
+
+  has_many :matchings
+  has_many :matches, through: :matchings, souce: :match
 end
