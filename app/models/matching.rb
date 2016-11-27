3fc987ca1fb760991d4cff2047018b71f7eccddb
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-26 23:00:02 -0800

    Add dependent destroy to matchings

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -3,5 +3,5 @@ class Matching < ApplicationRecord
 
   #Primary key must be set so Rails doesn't default to id
   belongs_to :summoner, primary_key: :summoner_id
-  belongs_to :match, primary_key: :match_id
+  belongs_to :match, primary_key: :match_id, dependent: :destroy
 end
