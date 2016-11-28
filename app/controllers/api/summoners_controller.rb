af6df1ba502cdafcff51ed00b98e4bf6dd9da7ce
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-28 00:19:58 -0800

    Installed sucker_punch

--- app/controllers/api/summoners_controller.rb
+++ app/controllers/api/summoners_controller.rb
@@ -3,14 +3,15 @@ class Api::SummonersController < ApplicationController
   #Attempt to find summoner in local DB. If none found, fetch and create from API.
   #Creates most recent (< 20) matches for this player.
   def find_or_create
+    byebug
     @summoner = Summoner.find_by(name: params[:name])
     if !@summoner
       @summoner = Summoner.create_summoner(params[:name])
-      render :show
       Match.fetch_matches(@summoner, {
           offset: 0,
           limit: 20
         })
+      render :show
     end
   end
 end
