aa1916cae281bf1280fd96a7c487e32bdaabaca1
Sonik Jhang <sonik.jhang@utexas.edu>
2016-11-29 12:35:33 -0800

    component start

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Splash extends React.Component {
+  componentDidMount() {
+    $(document).scrollTop(0);
+  }
+  
+  handleSubmit() {
+
+  }
+
   render () {
     return(
       <splash className="splash-page">
@@ -19,6 +27,8 @@ class Splash extends React.Component {
               </button>
             </form>
           </section>
+        </div>
+        <div className="second-div">
           <section className="guide">
             <div className="title">
               <h2>Leaderboards and Profiles</h2>
@@ -32,7 +42,7 @@ class Splash extends React.Component {
                 </div>
                 <ul>
                   <li>Offer accurate ingame information</li>
-                  <li className="li">Utilize latest database technology to provide users with:</li>
+                  <li>Utilize latest database technology to provide users with:</li>
                   <li>Match Histories</li>
                   <li>Match Results</li>
                   <li>Leaderboards</li>
@@ -40,12 +50,18 @@ class Splash extends React.Component {
                 </ul>
               </div>
             </div>
-            <div className="item">
-              <div className="img">PIC 2</div>
+            <div className="item2">
+              <div className="img2"></div>
               <div className="desc">
-                <div></div>
+                <div>
+                  <fin>Leaderboard</fin>
+                  &nbsp; for &nbsp;
+                  <fin>all the leagues</fin>
+                </div>
                 <ul>
-                  <li></li>
+                  <li>All Challenger and Master tiers from top to bottom</li>
+                  <li>Allow users to check their most recent rankings </li>
+                  <li>Database optimized for smooth user experience</li>
                 </ul>
               </div>
             </div>
