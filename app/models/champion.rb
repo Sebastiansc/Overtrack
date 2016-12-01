56f126c1923eb458e0d761f4f76a8cc7351e7a57
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-12-01 10:14:28 -0800

    Add item, spell and champion data to matches index

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -2,12 +2,7 @@ class Champion < ActiveRecord::Base
   validates :info, :stats, :name, :champion_id, :image, :blurb, presence: true
   validates :champion_id, uniqueness: true
 
-  def in_match(matches)
-    champion_ids = []
-    matches.each do |match|
-      match.participants.each do |participant|
-       champion_ids << ['champion_id']
-    end
+  def self.in_match(champion_ids)
     Champion.where(champion_id: champion_ids)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   validates :name, :item_id, :description, presence: true
 
   extend ApiHelper
+
+  def self.in_match(item_ids)
+    Item.where(item_id: item_ids)
+  end
 end
