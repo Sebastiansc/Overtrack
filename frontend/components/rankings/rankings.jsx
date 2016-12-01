52ea3ff8bb12d1e7eabc49e2cf910161bb35f839
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-30 22:14:37 -0800

    Queues showing. Data looping indefinetly

--- frontend/components/rankings/rankings.jsx
+++ frontend/components/rankings/rankings.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 import RankingItem from './ranking_item';
 import Infinite from 'react-infinite';
 
 export default class Rankings extends React.Component {
   constructor(props){
     super(props);
-    this.offset = 0;
-    this.limit = 50;
     this.fetching = false;
     this.state = {summoners: [], over: false, isInfiniteLoading: false,};
+    this.paginationInit();
 
+  }
+
+  paginationInit(){
+    this.offset = 0;
+    this.limit = 50;
   }
 
   componentDidMount(){
@@ -20,7 +25,7 @@ export default class Rankings extends React.Component {
     this.setState({summoners: this.state.summoners.concat(entries)});
   }
 
-  // Ensures component updates state when revisiting
+  // Ensures component updates state when revisiting.
   componentWillMount(){
     if(this.props.rank.entries){
       const entries = this.throttleEntries(this.props.rank.entries);
@@ -30,13 +35,19 @@ export default class Rankings extends React.Component {
 
   // Ensures component updates state on page refresh or first enter(offset = 50)
   componentWillReceiveProps(newProps){
-    if(this.offset === 50 || !newProps.rank.entries) return;
-    this.fetching = false;
+    this.fetching = false; //Notify #currentBatch that slice has been updated.
+    if (!newProps.rank.entries) return; // Function also gets called on
+    //internal state change. newProps might be empty.
+
+    // First condition will be satisfied when changing routes
     if (newProps.rank.queue !== this.props.rank.queue){
+      this.paginationInit();
       this.setState({summoners: this.throttleEntries(newProps.rank.entries)});
+    } else if(this.offset === 50) {
+        return;
     } else {
-      const entries = this.throttleEntries(newProps.rank.entries);
-      this.updateSummoners(entries);
+        const entries = this.throttleEntries(newProps.rank.entries);
+        this.updateSummoners(entries);
     }
   }
 
@@ -80,6 +91,7 @@ export default class Rankings extends React.Component {
   // Infinite component is oddly calling function before this condition is
   // satisfied. Added conditional early return to account for this case
   handleInfiniteLoad(){
+    debugger;
     if(!this.state.summoners.length) return;
     this.setState({isInfiniteLoading: true});
     const newSummoners = this.currentBatch(this.props.rank.entries);
@@ -99,24 +111,32 @@ export default class Rankings extends React.Component {
   render(){
     window.that = this;
     return(
-      <Infinite elementHeight={21}
-        containerHeight={2750}
-        infiniteLoadBeginEdgeOffset={100}
-        useWindowAsScrollContainer
-        onInfiniteLoad={() => this.handleInfiniteLoad()}
-        loadingSpinnerDelegate={this.elementInfiniteLoad()}
-        isInfiniteLoading={this.state.isInfiniteLoading}
-        preloadAdditionalHeight={Infinite.containerHeightScaleFactor(2)}
-        className='rankings'>
-        {this.state.summoners.map((entry, idx) => {
-          return (
-            <RankingItem key={idx}
-              entry={entry}
-              idx={idx + 1}
-              tier={this.tier(idx)}/>
-          );
-        })}
-      </Infinite>
+      <main className='rankings'>
+        <div className='queues'>
+          <Link to='rankings'>RANKED_SOLO_5x5</Link>
+          <Link to='rankings/flex_sr'>RANKED_FLEX_SR</Link>
+          <Link to='rankings/flex_tt'>RANKED_FLEX_TT</Link>
+          <Link to='rankings/team_5x5'>RANKED_TEAM_5x5</Link>
+          <Link to='rankings/team_3x3'>RANKED_TEAM_3x3</Link>
+        </div>
+        <Infinite elementHeight={21}
+          containerHeight={2750}
+          infiniteLoadBeginEdgeOffset={100}
+          useWindowAsScrollContainer
+          onInfiniteLoad={() => this.handleInfiniteLoad()}
+          loadingSpinnerDelegate={this.elementInfiniteLoad()}
+          isInfiniteLoading={this.state.isInfiniteLoading}
+          preloadAdditionalHeight={Infinite.containerHeightScaleFactor(2)}>
+          {this.state.summoners.map((entry, idx) => {
+            return (
+              <RankingItem key={idx}
+                entry={entry}
+                idx={idx + 1}
+                tier={this.tier(idx)}/>
+            );
+          })}
+        </Infinite>
+      </main>
     );
   }
 }
