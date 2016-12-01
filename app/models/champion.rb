5c9a9aa06d49cccd9027a4f4756d4efe0a69ca05
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-12-01 08:28:07 -0800

    Add champions per match

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -2,8 +2,12 @@ class Champion < ActiveRecord::Base
   validates :info, :stats, :name, :champion_id, :image, :blurb, presence: true
   validates :champion_id, uniqueness: true
 
-  def in_match(match)
-    champions_ids = match.map{ |_, info| info['summoner']['champion_id'] }
-    Champion.where(champion_id: champions_ids)
+  def in_match(matches)
+    champion_ids = []
+    matches.each do |match|
+      match.participants.each do |participant|
+       champion_ids << ['champion_id']
+    end
+    Champion.where(champion_id: champion_ids)
   end
 end
